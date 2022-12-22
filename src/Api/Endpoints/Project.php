4bdae994abf2f3fd940679c98e03ec0b06a53750
Jason Rumney <jasonrumney@gmail.com>
2022-12-23 08:48:34 +0900

    Add an isMemberOf() method to Project API.
    This convenience method does a search for the named project with the current
    logged in user as member, and returns true if such a project exists, false
    otherwise.

--- src/Api/Endpoints/Project.php
+++ src/Api/Endpoints/Project.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pharavel\Api\Endpoints;
 
+use Illuminate\Support\Facades\Auth;
+
 use Pharavel\Api\Phorge;
 
 class Project
@@ -30,5 +32,22 @@ class Project
     public function edit($params)
     {
         return $this->client->post('project.edit', $params);
+    }
+
+    /**
+     * Check if the currently logged in user is a member of the named project
+     *
+     * This assumes that the user nickname is the same as their username in
+     * Phorge.  This assumption is valid if you use OAuth2 login from this
+     * package, but if you use alternative login methods, you will need to
+     * ensure that the usernames match.
+     *
+     * @param $project the name of the project to check
+     */
+    public function isMemberOf($project)
+    {
+        $uid = Auth::user()->nickname;
+        $projects = $this->search(['name' => $project, 'members' => [$uid]]);
+        return count($projects) > 0;
     }
 }
