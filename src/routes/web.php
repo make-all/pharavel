29b2e74e83289b348fab03d7eb5969253759bc22
Jason Rumney <jasonrumney@gmail.com>
2022-12-23 00:52:03 +0900

    Specify web middleware in routes.
    Seems this does not automatically benefit from the Laravel inclusion rules.

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Pharavel\Socialite\LoginController;
 
-Route::get('/auth/phorge/redirect', [LoginController::class, 'redirectToPhorge'])->name('phorgeLogin');
+Route::get('/auth/phorge/redirect', [LoginController::class, 'redirectToPhorge'])->middleware('web')->name('phorgeLogin');
 
-Route::get('/auth/phorge/callback', [LoginController::class, 'handlePhorgeCallback']);
+Route::get('/auth/phorge/callback', [LoginController::class, 'handlePhorgeCallback'])->middleware('web');
