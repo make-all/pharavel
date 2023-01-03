bd37631723867e405eee5612cddf6e4cb59b0660
Jason Rumney <jasonrumney@gmail.com>
2023-01-03 14:36:01 +0900

    Use relative namespace for API objects.
    Namespace was missing Api/, but the error message makes it clear we can
    simplify by using a relative namespace anyway.

--- src/Api/Phorge.php
+++ src/Api/Phorge.php
@@ -31,139 +31,139 @@ class Phorge
         switch($name)
         {
             case 'almanac.binding':
-                return new Pharavel\Endpoints\AlmanacBinding($this);
+                return new Endpoints\AlmanacBinding($this);
             case 'almanac.device':
-                return new Pharavel\Endpoints\AlmanacDevice($this);
+                return new Endpoints\AlmanacDevice($this);
             case 'almanac.interface':
-                return new Pharavel\Endpoints\AlmanacInterface($this);
+                return new Endpoints\AlmanacInterface($this);
             case 'almanac.namespace':
-                return new Pharavel\Endpoints\AlmanacNamespace($this);
+                return new Endpoints\AlmanacNamespace($this);
             case 'almanac.network':
-                return new Pharavel\Endpoints\AlmanacNetwork($this);
+                return new Endpoints\AlmanacNetwork($this);
             case 'almanac.service':
-                return new Pharavel\Endpoints\AlmanacService($this);
+                return new Endpoints\AlmanacService($this);
             case 'auth':
-                return new Pharavel\Endpoints\Auth($this);
+                return new Endpoints\Auth($this);
             case 'badge':
-                return new Pharavel\Endpoints\Badge($this);
+                return new Endpoints\Badge($this);
             case 'calendar.event':
-                return new Pharavel\Endpoints\CalendarEvent($this);
+                return new Endpoints\CalendarEvent($this);
             case 'chatlog':
-                return new Pharavel\Endpoints\Chatlog($this);
+                return new Endpoints\Chatlog($this);
             case 'conduit':
-                return new Pharavel\Endpoints\Conduit($this);
+                return new Endpoints\Conduit($this);
             case 'conpherence':
-                return new Pharavel\Endpoints\Conpherence($this);
+                return new Endpoints\Conpherence($this);
             case 'countdown':
-                return new Pharavel\Endpoints\Countdown($this);
+                return new Endpoints\Countdown($this);
             case 'dashboard.panel':
-                return new Pharavel\Endpoints\DashboardPanel($this);
+                return new Endpoints\DashboardPanel($this);
             case 'differential.changeset':
-                return new Pharavel\Endpoints\DifferentialChangeset($this);
+                return new Endpoints\DifferentialChangeset($this);
             case 'differential.diff':
-                return new Pharavel\Endpoints\DifferentialDiff($this);
+                return new Endpoints\DifferentialDiff($this);
             case 'differential.revision':
-                return new Pharavel\Endpoints\DifferentialRevision($this);
+                return new Endpoints\DifferentialRevision($this);
             case 'differential':
-                return new Pharavel\Endpoints\Differential($this);
+                return new Endpoints\Differential($this);
             case 'diffusion.commit':
-                return new Pharavel\Endpoints\DiffusionCommit($this);
+                return new Endpoints\DiffusionCommit($this);
             case 'diffusion.respository':
-                return new Pharavel\Endpoints\DiffusionRepository($this);
+                return new Endpoints\DiffusionRepository($this);
             case 'diffusion.uri':
-                return new Pharavel\Endpoints\DiffusionUri($this);
+                return new Endpoints\DiffusionUri($this);
             case 'diffusion':
-                return new Pharavel\Endpoints\Diffusion($this);
+                return new Endpoints\Diffusion($this);
             case 'drydock.authorization':
-                return new Pharavel\Endpoints\DrydockAuthorization($this);
+                return new Endpoints\DrydockAuthorization($this);
             case 'drydock.blueprint':
-                return new Pharavel\Endpoints\DrydockBlueprint($this);
+                return new Endpoints\DrydockBlueprint($this);
             case 'drydock.lease':
-                return new Pharavel\Endpoints\DrydockLease($this);
+                return new Endpoints\DrydockLease($this);
             case 'drydock.resource':
-                return new Pharavel\Endpoints\DrydockResource($this);
+                return new Endpoints\DrydockResource($this);
             case 'edge':
-                return new Pharavel\Endpoints\Edge($this);
+                return new Endpoints\Edge($this);
             case 'feed':
-                return new Pharavel\Endpoints\Feed($this);
+                return new Endpoints\Feed($this);
             case 'file':
-                return new Pharavel\Endpoints\File($this);
+                return new Endpoints\File($this);
             case 'flag':
-                return new Pharavel\Endpoints\Flag($this);
+                return new Endpoints\Flag($this);
             case 'harbormaster.artifact':
-                return new Pharavel\Endpoints\HarbormasterArtifact($this);
+                return new Endpoints\HarbormasterArtifact($this);
             case 'harbormaster.build':
-                return new Pharavel\Endpoints\HarbormasterBuild($this);
+                return new Endpoints\HarbormasterBuild($this);
             case 'harbormaster.buildable':
-                return new Pharavel\Endpoints\HarbormasterBuildable($this);
+                return new Endpoints\HarbormasterBuildable($this);
             case 'harbormaster.buildplan':
-                return new Pharavel\Endpoints\HarbormasterBuildplan($this);
+                return new Endpoints\HarbormasterBuildplan($this);
             case 'harbormaster.log':
-                return new Pharavel\Endpoints\HarbormasterLog($this);
+                return new Endpoints\HarbormasterLog($this);
             case 'harbormaster.step':
-                return new Pharavel\Endpoints\HarbormasterStep($this);
+                return new Endpoints\HarbormasterStep($this);
             case 'harbormaster.target':
-                return new Pharavel\Endpoints\HarbormasterTarget($this);
+                return new Endpoints\HarbormasterTarget($this);
             case 'harbormaster':
-                return new Pharavel\Endpoints\Harbormaster($this);
+                return new Endpoints\Harbormaster($this);
             case 'internal.commit':
-                return new Pharavel\Endpoints\InternalCommit($this);
+                return new Endpoints\InternalCommit($this);
             case 'LegalpadDocument':
-                return new Pharavel\Endpoints\LegalpadDocument($this);
+                return new Endpoints\LegalpadDocument($this);
             case 'LegalpadSignature':
-                return new Pharavel\Endpoints\LegalpadSignature($this);
+                return new Endpoints\LegalpadSignature($this);
             case 'macro':
-                return new Pharavel\Endpoints\Macro($this);
+                return new Endpoints\Macro($this);
             case 'maniphest.priority':
-                return new Pharavel\Endpoints\ManiphestPriority($this);
+                return new Endpoints\ManiphestPriority($this);
             case 'maniphest.status':
-                return new Pharavel\Endpoints\ManiphestStatus($this);
+                return new Endpoints\ManiphestStatus($this);
             case 'maniphest':
-                return new Pharavel\Endpoints\Maniphest($this);
+                return new Endpoints\Maniphest($this);
             case 'owners':
-                return new Pharavel\Endpoints\Owners($this);
+                return new Endpoints\Owners($this);
             case 'packages.package':
-                return new Pharavel\Endpoints\PackagesPackage($this);
+                return new Endpoints\PackagesPackage($this);
             case 'packages.publisher':
-                return new Pharavel\Endpoints\PackagesPublisher($this);
+                return new Endpoints\PackagesPublisher($this);
             case 'packages.version':
-                return new Pharavel\Endpoints\PackagesVersion($this);
+                return new Endpoints\PackagesVersion($this);
             case 'passphrase':
-                return new Pharavel\Endpoints\Passphrase($this);
+                return new Endpoints\Passphrase($this);
             case 'paste':
-                return new Pharavel\Endpoints\Paste($this);
+                return new Endpoints\Paste($this);
             case 'phame.blog':
-                return new Pharavel\Endpoints\PhameBlog($this);
+                return new Endpoints\PhameBlog($this);
             case 'phame.post':
-                return new Pharavel\Endpoints\PhamePost($this);
+                return new Endpoints\PhamePost($this);
             case 'phequent':
-                return new Pharavel\Endpoints\Phrequent($this);
+                return new Endpoints\Phrequent($this);
             case 'phid':
-                return new Pharavel\Endpoints\Phid($this);
+                return new Endpoints\Phid($this);
             case 'phriction.content':
-                return new Pharavel\Endpoints\PhrictionContent($this);
+                return new Endpoints\PhrictionContent($this);
             case 'phriction.document':
-                return new Pharavel\Endpoints\PhrictionDocument($this);
+                return new Endpoints\PhrictionDocument($this);
             case 'Phriction':
-                return new Pharavel\Endpoints\Phriction($this);
+                return new Endpoints\Phriction($this);
             case 'phurls':
-                return new Pharavel\Endpoints\Phurls($this);
+                return new Endpoints\Phurls($this);
             case 'portal':
-                return new Pharavel\Endpoints\Portal($this);
+                return new Endpoints\Portal($this);
             case 'project.column':
-                return new Pharavel\Endpoints\ProjectColumn($this);
+                return new Endpoints\ProjectColumn($this);
             case 'project':
-                return new Pharavel\Endpoints\Project($this);
+                return new Endpoints\Project($this);
             case 'remarkup':
-                return new Pharavel\Endpoints\Remarkup($this);
+                return new Endpoints\Remarkup($this);
             case 'slowvote.poll':
-                return new Pharavel\Endpoints\SlowvotePoll($this);
+                return new Endpoints\SlowvotePoll($this);
             case 'token':
-                return new Pharavel\Endpoints\Token($this);
+                return new Endpoints\Token($this);
             case 'transaction':
-                return new Pharavel\Endpoints\Transaction($this);
+                return new Endpoints\Transaction($this);
             case 'user':
-                return new Pharavel\Endpoints\User($this);
+                return new Endpoints\User($this);
             default:
                 throw new InvalidArgumentException(sprintf('Undefined API called: %s', $name));
         }
